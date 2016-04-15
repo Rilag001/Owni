01271189aae8223797d16537832fe1e2c8c3683e
Rilag001 <rickylagerkvist@hotmail.com>
2016-04-15 15:59:06 +0200

    Login funktionen ska nu fungera bättre. Lyssnar om använderen fortfarande är inloggad  och öppnar LoginActivity om s¨ej är fallet

--- app/src/main/java/se/rickylagerkvist/owni/ui/MainActivity.java
+++ app/src/main/java/se/rickylagerkvist/owni/ui/MainActivity.java
@@ -91,7 +91,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mS ectionsPagerAdapter);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
 
         // set icons for tabs (with two states: seletcted and unselected)
         tabLayout = (TabLayout) findViewById(R.id.tabs);
